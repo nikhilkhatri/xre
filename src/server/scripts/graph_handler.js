ab96c550d2b6ef06769c3ec7749a645efd26471a
nikhilkhatri <nikhilkhatri97@gmail.com>
2018-11-10 18:07:00 +0530

    added front end using cytoscape

---
+++ src/server/scripts/graph_handler.js
@@ -0,0 +1,93 @@
+
+
+// each node has additional attrib :
+	// alloced : [true, false]
+
+find_entity = function(id){
+	var l = graph.elements.length;
+
+	for(var i = 0; i<l; i ++){
+		if (graph.elements[i].data.id == id){
+			return i;
+		}
+	}
+	return -1;
+}
+
+add_node = function(id, alloced){
+	var l = graph.elements.length;
+
+	for(var i = 0; i<l; i ++){
+		if (graph.elements[i].data.id === id){
+			graph.elements[i].data.alloced = alloced;
+			return ;
+		}
+	}
+	graph.elements.push(
+		{
+			"data": {"id": id, "alloced": alloced}
+		}
+	);
+	console.log("pushed node");
+	console.log(graph.elements);
+}
+
+add_edge = function(id, src, dest){
+	var l = graph.elements.length;
+
+	for(var i = 0; i<l; i ++){
+		if (graph.elements[i].data.id == id){
+			graph.elements[i].data = {"id": id, "source": src, "target": dest};
+			return ;
+		}
+	}
+
+	graph.elements.push(
+		{
+			"data": {"id": id, "source": src, "target": dest}
+		}
+	);
+	console.log("pushed edge");
+	console.log(graph.elements);
+}
+
+remove_edge = function(index){
+	graph.elements.splice(index, 1);
+}
+
+graph = {
+
+	container: document.getElementById('dagrediv'), // container to render in
+
+	elements: [ // list of graph elements to start with
+	],
+
+	style: [ // the stylesheet for the graph
+		{
+			selector: 'node',
+			style: {
+				'background-color': '#666',
+				'label': 'data(id)'
+			}
+		},
+
+		{
+			selector: 'edge',
+			style: {
+				'width': 1,
+				'line-color': '#f00',
+				'mid-target-arrow-color': '#00f',
+				'mid-target-arrow-shape': 'triangle',
+				'label': function(ele){
+					// console.log(ele);
+					return ele._private.data.id.split("_")[1];
+				}
+			}
+		}
+	],
+
+	layout: {
+		name: 'grid',
+		rows: 1
+	}
+}
---
+++ src/server/server_main.py
@@ -0,0 +1,94 @@
+import sys
+from flask import Flask, Response
+from flask_cors import CORS
+import time
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+
+def clog_to_json(clog):
+	clog = clog.split(":")
+
+	if(clog[0] == "N"):
+		# malloc / calloc node
+		obj = dict()
+		obj["sigtype"] = "SIGALOC"
+		obj["alloctype"] = "node"
+
+		if(clog[1] == "H"):
+			obj["source_region"] = "heap"
+			obj["source_addr"] = int(clog[2], 16)
+			obj["source_index"] = int(clog[3])
+			obj["dest"] =int(clog[4], 16)
+		else:
+			obj["source_region"] = "other"
+			obj["source_addr"] = int(clog[2], 16)
+			obj["dest"] = int(clog[3], 16)
+		return obj
+
+	if(clog[0] == "D"):
+		obj = dict()
+		obj["sigtype"] = "SIGALOC"
+		obj["alloctype"] = "data"
+		
+		if(clog[1] == "H"):
+			obj["source_region"] = "heap"
+			obj["source_addr"] = int(clog[2], 16)
+			obj["source_index"] = int(clog[3])
+			obj["dest"] = int(clog[4], 16)
+		else:
+			obj["source_region"] = "other"
+			obj["source_addr"] = int(clog[2], 16)
+			obj["dest"] = int(clog[3], 16)
+		return obj
+
+	if(clog[0] == "F"):
+		obj = dict()
+		obj["sigtype"] = "SIGFREE"
+		obj["addr"] = int(clog[1], 16)
+
+		return obj
+
+	if(clog[0] == "C"):
+		obj = dict()
+		obj["sigtype"] = "SIGCOPY"
+
+		if(clog[1] == "H"):
+			obj["source_region"] = "heap"
+			obj["source_addr"] = int(clog[2], 16)
+			obj["source_index"] = int(clog[3])
+			obj["dest"] = int(clog[4], 16)
+		else:
+			obj["source_region"] = "other"
+			obj["source_addr"] = int(clog[2], 16)
+			obj["dest"] = int(clog[3], 16)
+		return obj
+
+	else:
+		return "{\"FAILURE\"}"
+
+
+def generate():
+	while(True):
+		try:
+			last_line = input()
+		except:
+			yield "\n"
+			print("DONE")
+			return
+		else:
+			print(last_line + "\n")
+			yield json.dumps(clog_to_json(last_line)) + "\n"
+			time.sleep(0.1)
+
+
+@app.route('/')
+def stream_response():
+	return Response(generate())
+
+
+if __name__ == "__main__":
+	# generate()
+	app.run(port=5000)
